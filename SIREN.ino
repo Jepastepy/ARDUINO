f977a99aee71066c605a13a9648a5b95d37ae910
Jepastepy <146868716+Jepastepy@users.noreply.github.com>
2026-02-21 22:40:18 +0530

    Add files via upload

---
+++ SIREN.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(0,OUTPUT);
+  pinMode(6,OUTPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(0,HIGH);
+  delay(70);
+  digitalWrite(0,LOW);
+  delay(70);
+digitalWrite(6,HIGH);
+  delay(70);
+  digitalWrite(6,LOW);
+  delay(70);
+
+
+}
